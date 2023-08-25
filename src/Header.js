fcb16c755c0009d2a720c5515ea11ce9a5d2786e
Konstantinos <kostasmasters@gmail.com>
2023-08-24 20:42:07 -0400

    images fixed

--- src/Header.js
+++ src/Header.js
@@ -5,26 +5,26 @@ const Header = () => {
     <div className="headerContainer">
       <div className="sideProfileContainer">
         <img
-          src="./images/nick g05732.JPG"
+          src="./images/nick g05732.jpg"
           alt="sideProfile1"
           style={{ width: "25%" }}
           className="HeaderImages"
         />
         <img
-          src="./images/Nick Gabriel Djing in Barce solo shot2.JPG"
+          src="./images/Nick Gabriel Djing in Barce solo shot2.jpg"
           alt="sideProfile2"
           style={{ width: "25%" }}
           className="HeaderImages"
         />
         <img
-          src="./images/EDE53B86-3EAD-4C0A-9BC8-5218FADE153E4.JPG"
-          alt="sideProfile4"
+          src="./images/EDE53B86-3EAD-4C0A-9BC8-5218FADE153E4.jpg"
+          alt="sideProfile3"
           style={{ width: "25%" }}
           className="HeaderImages"
         />
         <img
-          src="./images/nickg05042.JPG"
-          alt="sideProfile5"
+          src="./images/nickg05042.jpg"
+          alt="sideProfile4"
           style={{ width: "25%" }}
           className="HeaderImages"
         />
